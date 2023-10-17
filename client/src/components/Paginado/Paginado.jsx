58033d3c25462031ef208a2daecfc87e00b8ef90
andresmarincelis <andresmarincelis@gmail.com>
2023-10-17 10:57:29 -0400

    Paginado y diseño mejorados

--- client/src/components/Paginado/Paginado.jsx
+++ client/src/components/Paginado/Paginado.jsx
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from "react-redux";
 import Card from "../Card/Card";
-
 import style from "./Paginado.module.css";
 
-
 const Paginado = () => {
-
     const countries = useSelector(state => state.countries.filtrados);
     const [filteredCountries, setFilteredCountries] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
@@ -28,27 +25,52 @@ const Paginado = () => {
         setCurrentPage((prevPage) => prevPage + 1);
     };
 
+    const goToFirstPage = () => {
+        setCurrentPage(1);
+    };
+
+    const goToLastPage = () => {
+        const totalPages = Math.ceil(filteredCountries.length / COUNTRIES_PER_PAGE);
+        setCurrentPage(totalPages);
+    };
+
     return (
-        <div className={style.cardList}>
-            {currentCountries.map((country) => (
-                <Card id={country.id} key={country.id} country={country} />
-            ))}
-            
-            <div>
-            <div>
-            <button>Página {currentPage}</button>
+        <div>
+            <div className={style.cardList}>
+                {currentCountries.map((country) => (
+                    <Card id={country.id} key={country.id} country={country} />
+                ))}
             </div>
+
+            <div className={style.buttonContainer}>
                 <button
                     className={style.button}
-                    onClick={prevHandler}
-                    disabled={currentPage === 1}>
-                    {"<"}
+                    onClick={goToFirstPage}
+                    disabled={currentPage === 1}
+                >
+                    {"Primera Página"}
                 </button>
                 <button
                     className={style.button}
+                    onClick={prevHandler}
+                    disabled={currentPage === 1}
+                >
+                    {"Anterior"}
+                </button>
+                <button className={style.button}>Página {currentPage}</button>
+                <button
+                    className={style.button}
                     onClick={nextHandler}
-                    disabled={indexOfLastPage >= filteredCountries.length}>
-                    {">"}
+                    disabled={indexOfLastPage >= filteredCountries.length}
+                >
+                    {"Siguiente"}
+                </button>
+                <button
+                    className={style.button}
+                    onClick={goToLastPage}
+                    disabled={indexOfLastPage >= filteredCountries.length}
+                >
+                    {"Última Página"}
                 </button>
             </div>
         </div>
